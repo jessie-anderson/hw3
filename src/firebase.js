e8f7dbeb5c14397197bcca65efc234dcf2b765bc
Jessie Anderson <Jessie@jessies-mbp-2.wireless.dartmouth.edu>
2016-07-24 20:09:10 -0400

    added comments

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 import Firebase from 'firebase';
-import Immutable from 'immutable';
 
 // Initialize Firebase
 const config = {
@@ -20,6 +19,7 @@ export function updateInfo(attribute, callback) {
   });
 }
 
+// push new note to database
 export function addNote(note, defaultX, defaultY, highestIndex) {
   database.ref('notes').push(note);
   database.ref('defaultX').set(defaultX + 5);
@@ -27,14 +27,18 @@ export function addNote(note, defaultX, defaultY, highestIndex) {
   database.ref('highestIndex').set(highestIndex + 1);
 }
 
+// delete note from database
 export function deleteNote(id) {
   database.ref('notes').child(id).remove();
 }
 
+// edit the body of the note with the given id, push change to database
 export function editBody(id, newBody) {
   database.ref('notes').child(id).update({ body: newBody });
 }
 
+// delete all notes from database and reset other state variables
+// basically set app back to initial state
 export function deleteAllNotes() {
   database.ref('notes').set({});
   database.ref('defaultX').set(20);
@@ -42,10 +46,12 @@ export function deleteAllNotes() {
   database.ref('highestIndex').set(0);
 }
 
+// change edit status of note with given id, push change to database
 export function changeEditStatus(id, editStatus) {
   database.ref('notes').child(id).update({ isEditing: editStatus });
 }
 
+// update all z indices of notes according to which one needs to come to front
 export function updateZIndices(notes, highestIndex, pressedIndex, noteId) {
   notes.forEach((note, id) => {
     if ((notes.get(id).zIndex >= pressedIndex) && (id !== noteId)) {
@@ -56,6 +62,7 @@ export function updateZIndices(notes, highestIndex, pressedIndex, noteId) {
   });
 }
 
+// update position of note with given id when dragged
 export function updatePosition(id, x, y) {
   database.ref('notes').child(id).update({ x, y });
 }
